fd7dce9fdb67c058770afc49a5956db44a0d0edf
Ryan Crowley <dacrowlah@gmail.com>
2023-05-23 13:09:56 -0700

    change definition of valid impression

--- src/main/java/com/iheartjane/processors/AdResponder.java
+++ src/main/java/com/iheartjane/processors/AdResponder.java
@@ -2,11 +2,17 @@ package com.iheartjane.processors;
 
 import com.iheartjane.models.AdResponse;
 import com.iheartjane.models.Campaign;
+import com.iheartjane.services.ImpressionService;
 import jakarta.inject.Singleton;
 import java.util.UUID;
 
 @Singleton
 public class AdResponder {
+  private final ImpressionService impressionService;
+  public AdResponder(ImpressionService impressionService) {
+    this.impressionService = impressionService;
+  }
+
   private static final String IMPRESSION_URL_FORMAT = "http://localhost:8000/impression?campaignId=%s&impressionId=%s";
   public AdResponse accept(Campaign campaign) {
 
@@ -20,6 +26,9 @@ public class AdResponder {
   private String buildImpressionUrl(Campaign campaign) {
     var impressionId = UUID.randomUUID().toString();
     var campaignId = campaign.getCampaignId();
+
+    impressionService.recordSentImpression(campaignId, impressionId);
+
     return String.format(IMPRESSION_URL_FORMAT, campaignId, impressionId);
   }
 }
--- src/test/java/com/iheartjane/controllers/ImpressionControllerTest.java
+++ src/test/java/com/iheartjane/controllers/ImpressionControllerTest.java
@@ -3,6 +3,7 @@ package com.iheartjane.controllers;
 import static com.iheartjane.fixtures.Campaigns.TARGET_KEYWORDS;
 import static com.iheartjane.fixtures.Campaigns.validCampaign;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.iheartjane.models.AdRequest;
 import com.iheartjane.services.CampaignService;
@@ -12,6 +13,9 @@ import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -41,7 +45,7 @@ public class ImpressionControllerTest {
   }
 
   @Test
-  public void testValidImpression() {
+  public void testValidImpression() throws MalformedURLException {
     var campaign = validCampaign();
     var adRequest = new AdRequest();
     adRequest.setKeywords(TARGET_KEYWORDS);
@@ -49,7 +53,13 @@ public class ImpressionControllerTest {
     campaignService.addCampaign(campaign);
 
     var adResponse = adDecisionController.getAd(adRequest).body();
-    var response = impressionController.impression(adResponse.getCampaignId(), fakeUUID);
+    var impressionId = getImpressionId(adResponse.getImpressionUrl());
+    assertFalse(impressionId.isEmpty());
+
+    var response = impressionController.impression(
+        adResponse.getCampaignId(),
+        impressionId.get()
+    );
 
     assertEquals(HttpStatus.OK, response.status());
   }
@@ -63,7 +73,7 @@ public class ImpressionControllerTest {
   }
 
   @Test
-  public void testValidImpressionDoesNotGetDoubleCounted() {
+  public void testValidImpressionDoesNotGetDoubleCounted() throws MalformedURLException {
     var campaign = validCampaign();
     var adRequest = new AdRequest();
     adRequest.setKeywords(TARGET_KEYWORDS);
@@ -71,12 +81,32 @@ public class ImpressionControllerTest {
     campaignService.addCampaign(campaign);
 
     var adResponse = adDecisionController.getAd(adRequest).body();
-    var response = impressionController.impression(adResponse.getCampaignId(), fakeUUID);
+    var impressionId = getImpressionId(adResponse.getImpressionUrl());
+
+    assertFalse(impressionId.isEmpty());
+
+    var response = impressionController.impression(
+        adResponse.getCampaignId(),
+        impressionId.get()
+    );
 
     assertEquals(1, impressionService.getCampaignImpressions(adResponse.getCampaignId()));
     assertEquals(HttpStatus.OK, response.status());
 
     impressionController.impression(adResponse.getCampaignId(), fakeUUID);
     assertEquals(1, impressionService.getCampaignImpressions(adResponse.getCampaignId()));
+  }
+
+  private static Optional<String> getImpressionId(String impressionUrl) throws MalformedURLException {
+    var queryString = (new URL(impressionUrl)).getQuery();
+    String[] pairs = queryString.split("&");
+    for (var pair : pairs) {
+      String[] parts = pair.split("=");
+      if (parts[0].equals("impressionId")) {
+        return Optional.of(parts[1]);
+      }
+    }
+
+    return Optional.empty();
   }
 }
